55c137b5508eeb503890140555c8d441af4679ba
hiroto-hara <xre0605@gmail.com>
2024-06-23 21:17:32 +0900

    auto-import-practice1

--- app.vue
+++ app.vue
@@ -1,6 +1,14 @@
+<script>
+const count = ref(1)
+const countUp = () => {
+  count++
+}
+</script>
 <template>
   <div>
     <NuxtRouteAnnouncer />
+    <p>ああああああああああああああああ</p>
+    <p>{{ count }}</p>
     <NuxtWelcome />
   </div>
 </template>
