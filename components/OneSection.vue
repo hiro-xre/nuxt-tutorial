5bb81aebc9cdbe587c3b7b4017fa796e44bba14e
hiroto-hara <xre0605@gmail.com>
2024-07-16 21:31:36 +0900

    emitの復習

---
+++ components/OneSection.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+interface Props {
+  rand: number;
+}
+
+interface Emits {
+  (event: "createNewRand"): void;
+}
+
+defineProps<Props>();
+const emit = defineEmits<Emits>();
+
+const onNewRandButtonClick = (): void => {
+  emit("createNewRand");
+}
+</script>
+
+<template>
+  <section class="box">
+    <p>子の乱数を表示:{{ rand }}</p>
+    <button @click="onNewRandButtonClick">乱数を生成</button>
+  </section>
+</template>
+
+<style>
+.box {
+  border: green 1px solid;
+  margin: 10px;
+}
+</style>
